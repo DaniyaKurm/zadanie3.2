5611870b4c3ef432ec08ee4150d3c1d79aee6c83
Daniya <daniya.kurmakaeva@yandex.ru>
2023-07-11 00:10:46 +0300

    Initial commit

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,7 @@ public class Main {
     public static void main(String[] args) {
         BmiService service = new BmiService();
         int kilogram = 63;
-        float metre = 1,74F;
-        int metre = float metre;
+        double metre = 1.74;
         int index = service.calculate(metre, kilogram);
 
         System.out.println(index);
