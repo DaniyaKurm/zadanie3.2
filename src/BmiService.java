cc33ba883264551b6ed7cc2ebb3e02003df4d77b
Daniya <daniya.kurmakaeva@yandex.ru>
2023-07-11 00:12:35 +0300

    Initial commit

--- src/BmiService.java
+++ src/BmiService.java
@@ -1,8 +1,8 @@
 public class BmiService {
-    public int calculate(float metre, int kilogram) {
+    public int calculate(double metre, int kilogram) {
 
         int index;
-        index = (int) (kilogram / metre*metre);
+        index = (int) (kilogram / (metre * metre));
         {
         }
         return index;
